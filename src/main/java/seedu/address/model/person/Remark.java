3592d374364354c8705f9ea03ca926dc7233e62b
Lemonsr <46766154+Lemonsr@users.noreply.github.com>
2021-09-17 04:36:00 +0800

    Fix Javadoc import errors

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,10 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructor for the remark class.
+     * @param remark remark to be used.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
